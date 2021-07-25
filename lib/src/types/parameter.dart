18374061f4e07aecdbe0edfcf0bbc0128ffd3d68
Arish Khan <arishsultan104@gmail.com>
2021-07-25 22:01:46 +0500

    created [Parameter] object

---
+++ lib/src/types/parameter.dart
@@ -0,0 +1,43 @@
+enum ParameterStyle {
+  matrix,
+  label,
+  form,
+  simple,
+  spaceDelimited,
+  pipeDelimited,
+  deepObject
+}
+
+class Parameter {
+  final String? name;
+  final String $in;
+  final String? description;
+  final bool required;
+  final bool? deprecated;
+  final bool? allowEmptyValue;
+  final ParameterStyle? style;
+  final bool? explode;
+  final bool? allowReserved;
+
+  /// TODO(arish): link it to schema object.
+  final dynamic schema;
+  final dynamic example;
+  final dynamic examples;
+  final Map<String, dynamic>? content;
+
+  const Parameter({
+    required this.$in,
+    required this.required,
+    this.name,
+    this.description,
+    this.deprecated,
+    this.allowEmptyValue,
+    this.style,
+    this.explode,
+    this.allowReserved,
+    this.schema,
+    this.example,
+    this.examples,
+    this.content,
+  });
+}
